940e96117b6a4c844ce6993863080d2482e6de06
Pei <nuyiep@hotmail.com>
2024-04-12 18:55:58 +0800

    Can register new user

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 from .models import Task
 
@@ -18,10 +20,22 @@ class CustomLoginView(LoginView):
 	def get_success_url(self):
 		return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+	template_name = 'base/register.html'
+	form_class = UserCreationForm
+	redirect_authenticated_user = True
+	success_url = reverse_lazy('tasks')
+
 # Create your views here.
 class TaskList(LoginRequiredMixin, ListView):
 	model = Task
 	context_object_name = 'tasks'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['tasks'] = context['tasks'].filter(user=self.request.user)
+		context['count'] = context['tasks'].filter(complete=False).count()
+		return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
 	model = Task
@@ -30,8 +44,12 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
 	model = Task
-	fields = '__all__'
+	fields = ['title', 'description', 'complete']
 	success_url = reverse_lazy('tasks')
+
+	def form_invalid(self, form):
+		form.instance.user = self.request.user
+		return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
 	model = Task
